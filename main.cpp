51bfa0a631ec8533e6624fb564b282c956a72f60
kellymyrs <kelly.myrsilidi@gmail.com>
2019-01-10 23:56:11 +0200

    Still creating uj

--- main.cpp
+++ main.cpp
@@ -2,8 +2,7 @@
 
 using namespace std;
 
-int main(int argc,char* argv[]){
-	/*
+int main(int argc,char* argv[]){	
 	ifstream input_file, conf_file;
 	ofstream output_file;
 	int i,j,k = 4 ,L = 5,P,N,d,n_clusters,in,as,up,km = 0,validation = 0; // N = number of items(lines) in the file, d = number of coordinates (dimension)
@@ -26,11 +25,15 @@ int main(int argc,char* argv[]){
 	com_line_parser(argc,argv,input_file,output_file,validation);
 
 	read_input(input_file,users,N,d,P);
-	*/
+	
 	
 	map<string, double> lex_map; // <- map (word) -> value
 	vector<vector<string>> bc_vect;
 	read_lex(lex_map);
 	read_bitcoins(bc_vect);
+
+	for(i = 0 ; i <= users.size() ; i++){
+		users[i]->Create_U(bc_vect,lex_map);
+	}
 	return 1;
 }
