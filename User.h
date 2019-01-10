e8b6c72c3f54663f20f439e6dc417198d275041c
kellymyrs <kelly.myrsilidi@gmail.com>
2019-01-10 23:43:54 +0200

    Changes for creating uj

--- User.h
+++ User.h
@@ -16,18 +16,57 @@
 #include <cmath>
 #include <unordered_map>
 #include <complex>
+#include "utils.h"
 
 using namespace std;
 
 struct User{
 	int id;
-  
+  	vector <double> u;
 	vector <bool> uex;
 	vector <struct Tweet*> tweets;
 
 	User(int i,struct Tweet* tweet){
 		id = i;
 		tweets.push_back(tweet);
+	}
+
+
+	void Create_U(vector <vector<string>>& coins,map<string, double>& lex){
+		int flag = 0;
+		int sum;
+
+		//calculate tweets' scores
+		for(int k = 0 ; k <= tweets.size() ; k++){
+			tweets[k]->Calculate_Score(lex);
+		}
+
+		for(int i = 0 ; i <= coins.size() ; i++){
+
+			sum = 0;
+			flag = 0;			
+			//search tweets
+			for(int k = 0 ; k <= tweets.size() ; k++){	
+
+				for(int j = 0 ; j <= coins[i].size() ; j++){
+
+					if(tweets[k]->Search_Tweet(coins[i][j])){
+						flag = 1; ////means that tweet talks about the coin
+						sum += tweets[k]->score;
+						break;
+					}
+				}
+
+			}
+
+			//if coin does't exist to any of the tweets
+			if(!flag){
+				u.push_back(-10000);
+			}
+			else{
+				u.push_back(sum);
+			}
+		}
 	}
 };
 
--- tweet.h
+++ tweet.h
@@ -16,17 +16,32 @@
 #include <cmath>
 #include <unordered_map>
 #include <complex>
+#include "utils.h"
 
 using namespace std;
 
 struct Tweet{
 	int id;
+	double score;
 	vector <string> words;
 
 	Tweet(int i,vector<string> v){
 		id = i;
+		score = 0;
 		words = v;
 	}
+
+	void Calculate_Score(map <string,double> dict){
+		for(int i = 0 ; i <= words.size() ; i++){
+			score += dict[words[i]];
+		}
+	}
+
+	int Search_Tweet(string word){
+		return find(words.begin(), words.end(), word) != word.end();
+	}
+
+
 };
 
 #endif
